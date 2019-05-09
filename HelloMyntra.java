a6c9eb2e5f693319915663966632d1b567f2ac1f
abhilashindulkar <abhilashindulkar@gmail.com>
2019-05-09 12:19:19 +0530

    First Commit

---
+++ HelloMyntra.java
@@ -0,0 +1,21 @@
+//i am writing my first java code for myntra project
+
+class HelloMyntra
+{
+	public static void main(String args[])
+	{
+		System.out.println("THE PROGRAM STARTS");
+		System.out.println("Hello......... Myntra");
+		System.out.println("THE PROGRAM ENDS");
+	}
+}
+
+/* simple code text
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
